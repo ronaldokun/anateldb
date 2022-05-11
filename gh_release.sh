88af86e08b82967572e9123736cc5ea90bd3bdc9
Ronaldo S.A. Batista <rsilva@anatel.gov.br>
2022-05-10 23:20:05 -0300

    updated automation script

--- gh_release.sh
+++ gh_release.sh
@@ -1,4 +1,8 @@
+source deactivate
 source activate db
-formatar_db dados --up_stel --up_radcom --up_mosaico
-git add -A && git commit -m ":bookmark: Base Atualizada" && git push
+formatar_db dados --up_stel true --up_radcom true --up_mosaico true
+scp dados/VersionFile.json .
+git add VersionFile.json
+git commit -m ":bookmark: Release $(cat ./dados/.version)"
+git push
 gh release create $(cat ./dados/.version) dados/*.xlsx --generate-notes
