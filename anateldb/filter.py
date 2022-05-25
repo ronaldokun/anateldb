fff18070da969eb334effb68b06f7868abe5e1de
Ronaldo S.A. Batista <rsilva@anatel.gov.br>
2022-05-24 21:29:06 -0300

    :recycle: Broken down queries into various modules according to function

--- anateldb/filter.py
+++ anateldb/filter.py
@@ -3,18 +3,22 @@
 __all__ = ['bump_version', 'get_modtimes', 'formatar_db']
 
 # Cell
-import os
 from pathlib import Path
 import json
-import pandas as pd
 from datetime import datetime
-from openpyxl import load_workbook
-from .query import *
-from .constants import console, APP_ANALISE
+
+import pandas as pd
 from fastcore.test import *
 from fastcore.script import call_parse, Param, store_true
+from pyarrow import ArrowInvalid
 from geopy.distance import geodesic
-from rich import print
+from rich.console import Console
+
+from .constants import APP_ANALISE
+from .read import read_base, read_aero
+from .merge import merge_aero
+from .format import df_optimize
+
 
 # Cell
 def bump_version(version, part=2):
@@ -23,95 +27,6 @@ def bump_version(version, part=2):
     for i in range(part + 1, 3):
         version[i] = "0"
     return ".".join(version)
-
-# Internal Cell
-def append_df_to_excel(
-    filename,
-    df,
-    sheet_name="Sheet1",
-    startrow=None,
-    truncate_sheet=False,
-    **to_excel_kwargs,
-):
-    """
-    Append a DataFrame [df] to existing Excel file [filename]
-    into [sheet_name] Sheet.
-    If [filename] doesn't exist, then this function will create it.
-
-    @param filename: File path or existing ExcelWriter
-                     (Example: '/path/to/file.xlsx')
-    @param df: DataFrame to save to workbook
-    @param sheet_name: Name of sheet which will contain DataFrame.
-                       (default: 'Sheet1')
-    @param startrow: upper left cell row to dump data frame.
-                     Per default (startrow=None) calculate the last row
-                     in the existing DF and write to the next row...
-    @param truncate_sheet: truncate (remove and recreate) [sheet_name]
-                           before writing DataFrame to Excel file
-    @param to_excel_kwargs: arguments which will be passed to `DataFrame.to_excel()`
-                            [can be a dictionary]
-    @return: None
-
-    Usage examples:
-
-    >>> append_df_to_excel('d:/temp/test.xlsx', df)
-
-    >>> append_df_to_excel('d:/temp/test.xlsx', df, header=None, index=False)
-
-    >>> append_df_to_excel('d:/temp/test.xlsx', df, sheet_name='Sheet2',
-                           index=False)
-
-    >>> append_df_to_excel('d:/temp/test.xlsx', df, sheet_name='Sheet2',
-                           index=False, startrow=25)
-
-    (c) [MaxU](https://stackoverflow.com/users/5741205/maxu?tab=profile)
-    """
-    # Excel file doesn't exist - saving and exiting
-    if not Path(filename).is_file():
-        df.to_excel(
-            filename,
-            sheet_name=sheet_name,
-            startrow=startrow if startrow is not None else 0,
-            **to_excel_kwargs,
-        )
-        return
-
-    # ignore [engine] parameter if it was passed
-    if "engine" in to_excel_kwargs:
-        to_excel_kwargs.pop("engine")
-
-    writer = pd.ExcelWriter(
-        filename, engine="openpyxl", mode="a", if_sheet_exists="replace"
-    )
-
-    # try to open an existing workbook
-    writer.book = load_workbook(filename)
-
-    # get the last row in the existing Excel sheet
-    # if it was not specified explicitly
-    if startrow is None and sheet_name in writer.book.sheetnames:
-        startrow = writer.book[sheet_name].max_row
-
-    # truncate sheet
-    if truncate_sheet and sheet_name in writer.book.sheetnames:
-        # index of [sheet_name] sheet
-        idx = writer.book.sheetnames.index(sheet_name)
-        # remove [sheet_name]
-        writer.book.remove(writer.book.worksheets[idx])
-        # create an empty sheet [sheet_name] using old index
-        writer.book.create_sheet(sheet_name, idx)
-
-    # copy existing sheets
-    writer.sheets = {ws.title: ws for ws in writer.book.worksheets}
-
-    if startrow is None:
-        startrow = 0
-
-    # write out the new sheet
-    df.to_excel(writer, sheet_name, startrow=startrow, **to_excel_kwargs)
-
-    # save the workbook
-    writer.save()
 
 # Cell
 def get_modtimes(pasta):
@@ -129,40 +44,39 @@ def get_modtimes(pasta):
         raise FileNotFoundError(f"Arquivo {radcom} não encontrado")
     if not (mosaico := pasta / 'mosaico.fth').is_file():
         raise FileNotFoundError(f"Arquivo {mosaico} não encontrado")
-    if not (icao := pasta / 'IcaoDB.xlsx').is_file():  # ICAO
+    if not (icao := pasta / 'icao.csv').is_file():  # ICAO
         raise FileNotFoundError(f"Arquivo {icao} não encontrado")
-    if not (pmec := pasta / 'PmecDB.xlsx').is_file():  # PMEC
+    if not (pmec := pasta / 'aisw.csv').is_file():  # PMEC
         raise FileNotFoundError(f"Arquivo {pmec} não encontrado")
-    if not (geo := pasta / 'GeoAiswebDB.xlsx').is_file():  # GEO
+    if not (geo := pasta / 'aisg.csv').is_file():  # GEO
         raise FileNotFoundError(f"Arquivo {geo} não encontrado")
     # Modificação
     mod_stel = datetime.fromtimestamp(stel.stat().st_mtime).strftime("%d/%m/%Y %H:%M:%S")
     mod_radcom = datetime.fromtimestamp(radcom.stat().st_mtime).strftime("%d/%m/%Y %H:%M:%S")
     mod_mosaico = datetime.fromtimestamp(mosaico.stat().st_mtime).strftime("%d/%m/%Y %H:%M:%S")
     mod_icao = pd.read_excel(icao, engine='openpyxl', sheet_name='ExtractDate').columns[0]
-    mod_pmec = pd.read_excel(pmec, engine='openpyxl', sheet_name='ExtractDate').columns[0]
-    mod_geo = pd.read_excel(geo, engine='openpyxl', sheet_name='ExtractDate').columns[0]
+    mod_aisw = pd.read_excel(pmec, engine='openpyxl', sheet_name='ExtractDate').columns[0]
+    mod_aisg = pd.read_excel(geo, engine='openpyxl', sheet_name='ExtractDate').columns[0]
     return {'STEL': mod_stel,
             'SRD': mod_radcom,
             'MOSAICO': mod_mosaico,
             'ICAO': mod_icao,
-            'AISW': mod_pmec,
-            'AISG': mod_geo}
+            'AISW': mod_aisw,
+            'AISG': mod_aisg}
 
 @call_parse
 def formatar_db(
     path: Param("Pasta onde salvar os arquivos", str),
-    up_stel: Param("Atualizar a base do Stel", store_true) = False,
-    up_radcom: Param("Atualizar a base do Radcom", store_true) = False,
-    up_mosaico: Param("Atualizar a base do Mosaico", store_true) = False,
+    up_base: Param("Atualizar as bases da Anatel", store_true) = False,
     up_icao: Param("Atualizar a base do ICAO", store_true) = False,
     up_pmec: Param("Atualizar a base do PMEC", store_true) = False,
     up_geo: Param("Atualizar a base do Geo", store_true) = False,
 ) -> None:
     dest = Path(path)
     dest.mkdir(parents=True, exist_ok=True)
-    console.print(":scroll:[green]Lendo as bases de dados...")
-    rd = read_base(path, up_stel, up_radcom, up_mosaico, up_icao)
+    console = Console()
+    console.print(":scroll:[green]Lendo as bases de dados da Anatel...")
+    rd = read_base(path, up_base)
     rd["Status"] = rd.Status.astype("string")
     rd["Classe"] = rd.Classe.astype("string")
     rd.loc[rd["Classe"].notna(), "Status"] = (
@@ -209,12 +123,11 @@ def formatar_db(
     console.print(":card_file_box:[green]Salvando os arquivos...")
     d = json.loads((dest / "VersionFile.json").read_text())
     mod_times = get_modtimes(path)
-    mod_times['ReleaseDate'] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    mod_times['ReleaseDate'] = datetime.today().strftime("%d/%m/%Y %H:%M:%S")
     with pd.ExcelWriter(f"{dest}/AnatelDB.xlsx", engine="xlsxwriter") as workbook:
         rd.to_excel(workbook, sheet_name="DataBase", index=False)
-        pd.DataFrame(columns=[mod_times['ReleaseDate']]).to_excel(workbook, sheet_name="ExtractDate", index=False)
     d["anateldb"]["Version"] = bump_version(d["anateldb"]["Version"])
     d['anateldb'].update(mod_times)
-    (dest / "VersionFile.json").write_text(json.dumps(d, indent=4))
+    json.dump(d, (dest / "VersionFile.json").open("w"))
     Path(dest / ".version").write_text(f"v{d['anateldb']['Version']}")
     console.print("Sucesso :zap:")
